d30cfa40ab84dc5a12da218b01879dffa65ca380
afteralec <github email address>
2020-07-21 22:32:32 -0700

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,36 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  #Instance methods that fetch data
+  def memberships
+    Membership.all.select { |membership| membership.gym == self }
+  end
+
+  def lifters
+    memberships.map(&:lifter)
+  end
+
+  def lifters_names
+    lifters.map(&:name)
+  end
+
+  def lifters_lift_total
+    lifters.map(&:lift_total).sum
   end
 end
